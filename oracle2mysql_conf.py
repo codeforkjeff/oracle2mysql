5503b8ab0753aaaffef55c521e212370d71e39a9
Jeff Chiu <jeff@codefork.com>
2015-02-20 11:06:38 -0500

    tweak env var names

--- oracle2mysql_conf.py
+++ oracle2mysql_conf.py
@@ -18,10 +18,10 @@ oracle_password = getpass.getpass("Oracle password: ")
 
 oracle = cx_Oracle.connect(oracle_username, oracle_password, "(DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = %s)(PORT = 1521))) (CONNECT_DATA = (SERVICE_NAME = rsrcutf8)))" % (oracle_host,))
 
-legacy_db_name = os.environ.get("SDBM_LEGACY_DB_NAME")
-legacy_db_user = os.environ.get("SDBM_LEGACY_DB_USER")
-legacy_db_password = os.environ.get("SDBM_LEGACY_DB_PASSWORD")
-legacy_db_host = os.environ.get("SDBM_LEGACY_DB_HOST")
+legacy_db_name = os.environ.get("SDBMSS_LEGACY_DB_NAME")
+legacy_db_user = os.environ.get("SDBMSS_LEGACY_DB_USER")
+legacy_db_password = os.environ.get("SDBMSS_LEGACY_DB_PASSWORD")
+legacy_db_host = os.environ.get("SDBMSS_LEGACY_DB_HOST")
 
 mysql = MySQLdb.connect(
     db=legacy_db_name,
